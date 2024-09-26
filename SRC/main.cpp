2f3610d3cadab841e0e8a8cb2139b19844fd2dbf
MajidAbdelilah <abdelilahmaid012@gmail.com>
2024-09-26 18:00:59 +0100

    trying to achive the best performance by combiyning cpu and gpu computation

--- SRC/main.cpp
+++ SRC/main.cpp
@@ -4,22 +4,32 @@
 #include <chrono>
 #include <sycl/sycl.hpp>
 #include "Matrix.hpp"
-#define N 10000000
-
+#define N 65535*100//100000000
+#include <thread>
 using namespace sycl;
+
+
+void thread_wait(queue q)
+{
+	q.wait();
+}
 
 int main(int arg_num, char **args)
 {
-	queue q;
-	if(arg_num > 1)
-	{
-		if(std::string(args[1]) == "cpu")
-			q = queue(cpu_selector_v);
-		else if(std::string(args[1]) == "gpu")
-			q = queue(gpu_selector_v);
-		else 
-			q = queue(default_selector_v);
-	}
+	queue qgpu;
+	queue qcpu;
+	// if(arg_num > 1)
+	// {
+	// 	if(std::string(args[1]) == "cpu")
+	// 		q = queue(cpu_selector_v);
+	// 	else if(std::string(args[1]) == "gpu")
+	// 		q = queue(gpu_selector_v);
+	// 	else 
+	// 		q = queue(default_selector_v);
+	// }
+	qcpu = queue(cpu_selector_v);
+	qgpu = queue(gpu_selector_v);
+	
 	// std::cout << q.get_device().get_info<ext::intel::info::device::gpu_eu_count>() << "\n";
 	// std::cout << q.get_device().get_info<ext::intel::info::device::gpu_eu_simd_width>() << "\n";
 	// std::cout << q.get_device().get_info<ext::intel::info::device::gpu_hw_threads_per_eu>() << "\n";
@@ -29,19 +39,28 @@ int main(int arg_num, char **args)
 	
 	// queue q2(gpu_selector_v);
 	// std::cout << q2.get_device().get_info<info::device::max_compute_units>() << "\n";
-	std::cout << q.get_device().get_info<info::device::native_vector_width_char>() << "\n";
-	std::cout << q.get_device().get_info<info::device::native_vector_width_short>() << "\n";
-	std::cout << q.get_device().get_info<info::device::native_vector_width_int>() << "\n";
-	std::cout << q.get_device().get_info<info::device::native_vector_width_long>() << "\n";
-	std::cout << q.get_device().get_info<info::device::native_vector_width_half>() << "\n"; 
-	std::cout << q.get_device().get_info<info::device::native_vector_width_float>() << "\n"; 
-	std::cout << q.get_device().get_info<info::device::native_vector_width_double>() << "\n"; 
 	
+	std::cout << qgpu.get_device().get_info<info::device::native_vector_width_char>() << "\n";
+	std::cout << qcpu.get_device().get_info<info::device::native_vector_width_char>() << "\n";	
+	std::cout << qgpu.get_device().get_info<info::device::native_vector_width_short>() << "\n";
+	std::cout << qcpu.get_device().get_info<info::device::native_vector_width_short>() << "\n";	
+	std::cout << qgpu.get_device().get_info<info::device::native_vector_width_int>() << "\n";
+	std::cout << qcpu.get_device().get_info<info::device::native_vector_width_int>() << "\n";	
+	std::cout << qgpu.get_device().get_info<info::device::native_vector_width_long>() << "\n";
+	std::cout << qcpu.get_device().get_info<info::device::native_vector_width_long>() << "\n";	
+	std::cout << qgpu.get_device().get_info<info::device::native_vector_width_half>() << "\n"; 
+	std::cout << qcpu.get_device().get_info<info::device::native_vector_width_half>() << "\n"; 	
+	std::cout << qgpu.get_device().get_info<info::device::native_vector_width_float>() << "\n"; 
+	std::cout << qcpu.get_device().get_info<info::device::native_vector_width_float>() << "\n"; 	
+	std::cout << qgpu.get_device().get_info<info::device::native_vector_width_double>() << "\n"; 
+	std::cout << qcpu.get_device().get_info<info::device::native_vector_width_double>() << "\n"; 	
 	
 	// Matrix4x4 *a = malloc_device<Matrix4x4>(N, q);
 	// Matrix4x4 *b = malloc_device<Matrix4x4>(N, q);
 	// Matrix4x4 *c = malloc_device<Matrix4x4>(N, q);
-	Vector3 *c = malloc_device<Vector3>(N, q);
+	Vector3 *cgpu = malloc_device<Vector3>(N/2, qgpu);
+	Vector3 *ccpu = malloc_device<Vector3>(N-N/2, qcpu);
+
 
 	Vector3 av(1, 1, 1);
 	Vector3 bv(2, 0, 2);
@@ -54,36 +73,58 @@ int main(int arg_num, char **args)
 	// e.push_back(q.fill(c, Matrix4x4(1.0f), N));
 	
 	// e.push_back(q.fill(c, Vector3(0, 0, 0), N));
-	int jobs = 0;
-	if(q.get_device().is_gpu())
-		jobs = q.get_device().get_info<ext::intel::info::device::gpu_eu_count>() * q.get_device().get_info<ext::intel::info::device::gpu_hw_threads_per_eu>();
-	else if(q.get_device().is_cpu())
-		jobs = q.get_device().get_info<info::device::max_compute_units>();
-	else
-		jobs = 4096;
-	std::cout << "jobs = " << jobs << "\n";
-	// q.wait();
+	// int jobs = 0;
+	// if(q.get_device().is_gpu())
+	// 	jobs = q.get_device().get_info<ext::intel::info::device::gpu_eu_count>() * q.get_device().get_info<ext::intel::info::device::gpu_hw_threads_per_eu>();
+	// else if(q.get_device().is_cpu())
+	// 	jobs = q.get_device().get_info<info::device::max_compute_units>();
+	// else
+	// 	jobs = 4096;
+	int jobs_cpu = qcpu.get_device().get_info<info::device::max_compute_units>();
+	int jobs_gpu = qgpu.get_device().get_info<ext::intel::info::device::gpu_eu_count>() * qgpu.get_device().get_info<ext::intel::info::device::gpu_hw_threads_per_eu>();
+	
+	std::cout << "jobs_cpu = " << jobs_cpu << "\n";
+	std::cout << "jobs_gpu = " << jobs_gpu << "\n";
+	qgpu.wait();
+	qcpu.wait();
+	
 	Matrix4x4 test(av, bv, cv, dv);
-	q.submit([&](handler &h){
+	qgpu.submit([&](handler &h){
 		// h.depends_on(e);
-		h.parallel_for(jobs, [=](item<1> it){
-			for(unsigned long i = it[0]; i < N; i += it.get_range(0))
+		h.parallel_for(jobs_gpu, [=](item<1> it){
+			for(unsigned long i = it[0]; i < N/2; i += it.get_range(0))
 			{
-				c[i] = test * Vector3(1.0f, 1.0f, 1.0f);
+				cgpu[i] = test * Vector3(1.0f, 1.0f, 1.0f);
 			}
 		});
 	});
+	qcpu.submit([&](handler &h){
+		// h.depends_on(e);
+		h.parallel_for(jobs_cpu, [=](item<1> it){
+			for(unsigned long i = it[0]; i < N-N/2; i += it.get_range(0))
+			{
+				ccpu[i] = test * Vector3(1.0f, 1.0f, 1.0f);
+			}
+		});
+	});
+	
+	std::thread th_gpu(thread_wait, qgpu);
+	std::thread th_cpu(thread_wait, qcpu);
+	
 
 	std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-	q.wait();
+	th_gpu.join();
+	th_cpu.join();
 	std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
 
 	std::cout << "Time difference = " << std::chrono::duration_cast<std::chrono::microseconds>(end - begin).count() << "[ms]" << std::endl;
 
 	// Matrix4x4 *result = new Matrix4x4[N];
 	Vector3 *result = new Vector3[N];
-	q.copy(c, result, N);
-	q.wait();
+	qgpu.copy(cgpu, result, N/2);
+	qcpu.copy(ccpu, result + N/2, N-N/2);
+	qgpu.wait();
+	qcpu.wait();
 
 	Vector3 v = (result[0]);
 	// Vector3 v = result[0] * Vector3(1.0f, 1.0f, 1.0f);
